8757da3c827eca1588771f9c81364291c68f7601
xxHMT <112229204+xxHMT@users.noreply.github.com>
2024-10-09 20:08:28 +0700

    bth

--- obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca86362ee66e39966807b406a45e72460605896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03a5980a14051145dc6ad93d7c00609c49ce257")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
